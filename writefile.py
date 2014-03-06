63c9b3c2e8debdd6a2591041194bf7a35086f865
psaux0 <russell.susu@gmail.com>
2014-03-06 00:06:09 -0500

    fix a small bug

--- writefile.py
+++ writefile.py
@@ -7,7 +7,6 @@ def writeShell(s):
     script=string.Template("""#!/bin/bash
 tmpdir=$$(pwd)
 sed -n -e '1,/^exit 0$$/!p' $$0> "$${tmpdir}/${name}.tar.gz" 2>/dev/null
-cd $$tmpdir
 tar zxvf ${name}.tar.gz
 rm ${name}.tar.gz
 exit 0
