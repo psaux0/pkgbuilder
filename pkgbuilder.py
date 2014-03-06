d1f099d371de1b9a4037620c562d1f1c90dcfc92
psaux0 <russell.susu@gmail.com>
2014-03-06 18:11:06 -0500

    Add source code compile function

--- pkgbuilder.py
+++ pkgbuilder.py
@@ -11,7 +11,7 @@ import fileinput
 
 bintype={'sh':'.sh','bin':'.bin','run':'.run'}
 
-def produceScript(path,ftype):
+def produceScript(path,ftype,src):
     """
      produce compiled script, binary data, cannot be opened
     """
@@ -21,36 +21,40 @@ def produceScript(path,ftype):
 	    subprocess.call(['tar','zcvf',tarballname,path])
     except e:
         print "Unknown error, cannot complie file"
-	
-	scriptname=writeShell(pkgname)
-	installfile_name=scriptname[:-3]+'_install'+bintype[ftype]
-	with open(installfile_name,'w') as ifn:
-	    for line in fileinput.input([scriptname,tarballname]):
-	        ifn.write(line)
-	
-	subprocess.call(['rm',tarballname])
-	subprocess.call(['rm',scriptname])
+
+    makeConfig=None
+    if src:
+        makeConfig=raw_input('Please enter configuration for makefile: ')
+
+    scriptname=writeShell(pkgname,makeConfig,src)
+    installfile_name=scriptname[:-3]+'_install'+bintype[ftype]
+    with open(installfile_name,'w') as ifn:
+        for line in fileinput.input([scriptname,tarballname]):
+            ifn.write(line)
+
+    subprocess.call(['rm',tarballname])
+    subprocess.call(['rm',scriptname])
 
 def main():
-    parser = optparse.OptionParser('usage: %prog '+'-f <target file> -t <type> default="sh"')
+    parser = optparse.OptionParser('usage: %prog '+'-f <target file> -t <type> default="sh" --from-src <True or False>')
     parser.add_option('-f', dest='tgtPath', type='string', help='specify the target file path')
     parser.add_option('-t', dest='compileType', type='string', help='specify the type to complie into',default="sh")
+    parser.add_option('--from-src', dest='fromsrc',action="store_true", help='specify whether needs to compile from source code',default=False)
     (options,args) = parser.parse_args()
     path=options.tgtPath
     ftype=options.compileType
+    src=options.fromsrc
 
 
-    p=path
-    t=ftype
-    if not os.path.exists(p):
+    if not os.path.exists(path):
         print "Error: Path doesn't exist"
         sys.exit(1)
-    if t not in bintype:
+    if ftype not in bintype:
         print "Error: Can only compile into bin,run or sh"
         sys.exit(1)
 	
     try:
-        produceScript(path,ftype)
+        produceScript(path,ftype,src)
     except TypeError:
         print "Error: wrong arguments type"
         parser.print_usage()
