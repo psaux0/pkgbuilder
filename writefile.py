da51b9197a5d98568bf47b014c8b52fe2b0a6eea
psaux0 <russell.susu@gmail.com>
2014-03-06 18:11:40 -0500

    Add source code compile shell script template

--- writefile.py
+++ writefile.py
@@ -3,7 +3,7 @@
 
 import string
 
-def writeShell(s):
+def writeShell(s,config,src):
     """
      Write the part for installation of a script
     """
@@ -14,9 +14,27 @@ tar zxvf ${name}.tar.gz
 rm ${name}.tar.gz
 exit 0
 """)
+    src_script=string.Template("""#!/bin/bash
+tmpdir=$$(pwd)/tmp_dir
+mkdir $$tmpdir
+sed -n -e '1,/^exit 0$$/!p' $$0> "$${tmpdir}/${name}.tar.gz" 2>/dev/null
+cd $$tmpdir
+tar zxvf ${name}.tar.gz
+rm ${name}.tar.gz                
+./configure $config
+make
+make install
+make clean
+echo "${tmpdir} can be removed"                               
+exit 0
+""")
     filename=s
     scriptname=''.join([filename,'.sh'])
     f=open(scriptname,'w')
-    f.write(script.substitute(name=filename))
+    r={'name':filename,'config':config}
+    if src:
+        f.write(src_script.substitute(r))
+    else:
+        f.write(script.substitute(name=r['name']))
     f.close()
     return scriptname
